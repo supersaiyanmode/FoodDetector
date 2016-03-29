6581ad63fd3dde4825c9a531e9ce5f01fa208097
Srivatsan Iyer <srriyer@indiana.edu>
2016-03-29 08:55:36 -0400

    updates.

--- src/EigenSVM.cpp
+++ src/EigenSVM.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <fstream>
+#include <sstream>
 
 #include <CImg.h>
 #include <EigenSVM.h>
@@ -25,7 +26,9 @@ void EigenSVM::preprocess(const Dataset& data) {
 	int cur_row = 0;
 
 	CImg<double> average_image(resizeX, resizeY);
-	CImg<double> all_data(total_images, resizeX * resizeY);
+	CImg<double> all_data(resizeX * resizeY, total_images);
+	average_image.fill(0.0);
+
 	for (Dataset::const_iterator it = data.begin(); it != data.end(); it++) {
 		std::string cur_food_name = it->first;
 		std::vector<std::string> files = it->second;
@@ -40,25 +43,27 @@ void EigenSVM::preprocess(const Dataset& data) {
 			//Put the image in the all_data variable.
 			CImg<double> unrolled = cur_image.unroll('x');
 			for (int i=0; i<unrolled.width(); i++) {
-				all_data(cur_row, i) = unrolled(i, 0);
+				all_data(i, cur_row) = unrolled(i, 0);
 			}
 			cur_row ++;
 		}
 	}
+	std::cout<<"Done with preprocess....\n";
 	average_image /= (double)total_images;
 
-	this->average_image = average_image;
+	average_image.display();
 
-	CImg<double> temp = average_image.resize(5, 5).unroll('x');
-	for (int i=0; i<temp.width(); i++)
-		std::cout<<temp(i, 0);
-	
-	CImg<double> covmat = all_data * all_data.get_transpose();
+	this->average_image = average_image;
+	average_image.save("average.png");
+
+	CImg<double> covmat = (all_data.get_transpose() * all_data) / total_images;
 	covmat.symmetric_eigen(this->eigen_values, this->eigen_vector);
 	
 	std::cout<<"Covmat dimension: "<<covmat.width()<<" "<<covmat.height()<<std::endl;
-	std::cout<<"Eigen vector size: "<<this->eigen_vector.size()<<std::endl;
+	std::cout<<"Eigen vector size: "<<this->eigen_vector.width()<<" "<<this->eigen_vector.height()<<std::endl;
+	std::cout<<"Eigen values size: "<<this->eigen_values.width()<<" "<<this->eigen_values.height()<<std::endl;
 
+	
 }
 
 std::vector<double> EigenSVM::get_feature_vector(const std::string& filename, bool is_train_mode) {
