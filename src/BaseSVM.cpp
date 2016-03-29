1d3995bd1ceb4556b080147a377f3a362002bdca
Srivatsan Iyer <srriyer@indiana.edu>
2016-03-28 23:22:04 -0400

    Adding config option for greyscale conversion of images.

--- src/BaseSVM.cpp
+++ src/BaseSVM.cpp
@@ -4,6 +4,7 @@
 #include <CImg.h>
 
 #include <BaseSVM.h>
+#include <Config.h>
 
 using namespace cimg_library;
 BaseSVM::BaseSVM(const std::vector<std::string>& cl, const std::string& wd): SVM(cl, wd) {
@@ -17,7 +18,7 @@ BaseSVM::~BaseSVM() {
 std::vector<double> BaseSVM::get_feature_vector(const std::string& filename, bool) {
 	CImg<double> input(filename.c_str());
 	CImg<double> resized = input.resize(40, 40, 1, 3);
-	bool force_grey = false;
+	int force_grey = config.get<int>("base.forcegrey");
 	if (resized.spectrum() == 3 && !force_grey) {
 		std::vector<double> result;
 		result.reserve(resized.width() * resized.height() * resized.spectrum());
